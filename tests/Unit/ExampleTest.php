83a8b80e4361778a94a8941b723fe8f6614010de
Jack Whiting <hi@jackwhiting.co.uk>
2018-03-18 22:04:20 +0000

    beginning modification of tests

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -12,8 +12,8 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testUnitCase()
     {
-        $this->assertTrue(true);
+
     }
 }
