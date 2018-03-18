2a508c6b643e5625f6272e4fe1001f83613308f7
Jack Whiting <hi@jackwhiting.co.uk>
2018-03-18 16:04:41 +0000

    adding ingredients table into the search

--- app/Http/Controllers/Api/V1/SearchController.php
+++ app/Http/Controllers/Api/V1/SearchController.php
@@ -6,33 +6,32 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Recipe;
 use App\Models\Tag;
+use App\Models\RecipeIngredient;
 
 class SearchController extends Controller
 {
     public function get(Request $request)
     {
         $search = Recipe::where('title', 'LIKE', '%' . $request->input('term') . '%')->get();
-
-        $search->each(function($item) {
-            $item->type = 'Recipe';
-        });
-
         $tags = Tag::where('title', 'LIKE', '%' . $request->input('term') . '%')->get();
+        $ingredients = RecipeIngredient::where('title', 'LIKE', '%' . $request->input('term') . '%')->get();
 
         $tags->each(function($item) {
             $item->type = 'category';
         });
 
-        $ingredients = Recipe::where('ingredients->title', 'LIKE', '%' . $request->input('term') . '%')->get();
+        $search->each(function($item) {
+            $item->type = 'Recipe';
+        });
 
+        $ingredients = $ingredients->unique('title');
         $ingredients->each(function($item) {
             $item->type = 'ingredient';
         });
 
-        $search = $search->merge($tags)
-                    ->merge($ingredients)
-                    ->take(10);
+        $search = $search->merge($tags);
+        $search = $search->merge($ingredients);
 
-        return response()->json($search);
+        return response()->json($search->take(10));
     }
 }
--- database/factories/RecipeIngredientFactory.php
+++ database/factories/RecipeIngredientFactory.php
@@ -3,8 +3,6 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\RecipeIngredient::class, function (Faker $faker) {
-    // $faker->addProvider(new Faker\Provider\Lorem($faker));
-    
     return [
         'quantity' => $faker->numberBetween(1, 500),
         'measurement' => $faker->randomElement(['g','ml','l','tbsp','tsp','']),
